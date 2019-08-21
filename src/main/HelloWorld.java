ef2079ab6153a3f06d668f77ec32fa33b70baab0
Propibe <matiaspippig@gmail.com>
2019-08-21 19:34:04 -0300

    ultima prueba

--- src/main/HelloWorld.java
+++ src/main/HelloWorld.java
@@ -4,7 +4,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		System.out.print("Hola mundo!");
-		System.out.println("Propibe configurado y desde eclipse");
+		System.out.println("Propibe configurado y desde eclipse ahhh");
 	}
 
 }
