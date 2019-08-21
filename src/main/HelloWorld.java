c05afb953cf65f480980b141c72a05b7a174d730
Propibe <matiaspippig@gmail.com>
2019-08-21 19:25:33 -0300

    Armado y preparado

--- src/main/HelloWorld.java
+++ src/main/HelloWorld.java
@@ -4,7 +4,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		System.out.print("Hola mundo!");
-		System.out.println("Propibe configurado");
+		System.out.println("Propibe configurado y desde eclipse");
 	}
 
 }
