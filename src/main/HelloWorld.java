23a99e0650ba4666fe9ac07b576cbd9df06a3bae
LUC45G <lucasgxnzalez@gmail.com>
2019-08-21 19:43:20 -0300

    Commit a pedido de propibe a ver si esto te anda

--- src/main/HelloWorld.java
+++ src/main/HelloWorld.java
@@ -5,6 +5,8 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.print("Hola mundo!");
 		System.out.println("Propibe configurado y desde eclipse ahhh");
+		
+		/* Lucas aca pusheando de nuevo ;D */
 	}
 
 }
