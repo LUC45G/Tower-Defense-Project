ffb5604f4526c130e3c84b8ba4dd89868a46da15
Pablo Barbero <pablobarbero220@gmail.com>
2019-08-22 10:42:32 -0300

    Update
    ab

--- src/main/HelloWorld.java
+++ src/main/HelloWorld.java
@@ -5,6 +5,8 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.print("Hola mundo!");
 		System.out.println("Propibe configurado y desde eclipse ahhh");
+		System.out.print("Pablo 123 probando 123");
+
 		
 		/* Lucas aca pusheando de nuevo ;D */
 		/*recibido*/
